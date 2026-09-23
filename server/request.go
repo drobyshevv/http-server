973aa3144077c6230244702ffb2eaa2619fb6a25
drobyshevv <grabensix666@mail.ru>
2026-09-23 21:48:21 +0300

    feat: добавлен парсер заголовков запроса

--- server/request.go
+++ server/request.go
@@ -25,8 +25,26 @@ func parseStartLine(startLine string) (string, string, string, error) {
 	return parts[0], parts[1], parts[2], nil
 }
 
-func parseHeaders() {
+func parseHeaders(reader *bufio.Reader) (map[string]string, error) {
+	headers := make(map[string]string)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
 
+		if line == "\r\n" {
+			break
+		}
+
+		parts := strings.SplitN(line, ":", 2)
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
+
+		headers[key] = value
+	}
+
+	return headers, nil
 }
 
 func parseBody() {
@@ -38,8 +56,16 @@ func parseRequest(reader *bufio.Reader) (*Request, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to read start line %w", err)
 	}
+
 	method, reqTarget, protocol, err := parseStartLine(startLine)
 	if err != nil {
 		return nil, err
 	}
+
+	headers, err := parseHeaders(reader)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse headers %w", err)
+	}
+
+	return &Request{}, nil
 }
