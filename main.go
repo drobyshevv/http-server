cdf44d6821421906a5b3d0a40e59ca10db3d064e
drobyshevv <grabensix666@mail.ru>
2026-09-24 12:44:48 +0300

    refactor: структура и конструктор сервера

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "github.com/drobyshevv/http-server/server"
 
 func main() {
-	server.Server()
+	cfg := server.Config{
+		Network: "tcp",
+		Port:    8080,
+	}
+	server := server.New(cfg)
+	server.Run()
 }
--- server/server.go
+++ server/server.go
@@ -6,32 +6,49 @@ import (
 	"net"
 )
 
-func Server() {
-	listenner, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		return
-	}
-	defer listenner.Close()
+type Server struct {
+	listener net.Listener
+	Cfg      Config
+}
 
-	fmt.Println("tcp server is started")
+type Config struct {
+	Network string
+	Port    int
+}
 
-	for {
-		conn, err := listenner.Accept()
-		if err != nil {
-			fmt.Printf("Failed connection: %s", err.Error())
-			continue
-		}
-
-		go getConnection(conn)
+func New(cfg Config) *Server {
+	return &Server{
+		Cfg: cfg,
 	}
 }
 
-func getConnection(conn net.Conn) {
+func (s *Server) Run() error {
+	listener, err := net.Listen(s.Cfg.Network, fmt.Sprintf(":%d", s.Cfg.Port))
+	if err != nil {
+		return fmt.Errorf("failed to start server: %w", err)
+	}
+
+	defer listener.Close()
+	s.listener = listener
+
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			continue
+		}
+
+		go worker(conn)
+	}
+
+}
+
+func worker(conn net.Conn) {
 	defer conn.Close()
 
 	reader := bufio.NewReader(conn)
 
-	res, _ := parseRequest(reader)
-	fmt.Println(string(res.Body))
+	res, err := parseRequest(reader)
+	if err != nil {
+		return
+	}
 }
